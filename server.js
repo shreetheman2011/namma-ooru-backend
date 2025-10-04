f4d0e2d679dd60cdcc290c134de2056929b07032
Shree Manickaraja <ShreeM@Manickarajas-MacBook-Pro.local>
2025-10-04 11:11:09 -0700

    new changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
-    .get("https://react-native-acepoint-backend.onrender.com", (res) => {
+    .get("https://https://namma-ooru-backend-q34n.onrender.com", (res) => {
       if (res.statusCode === 200) console.log("GET request sent succesfully");
       else console.log("GET request failed", res.statusCode);
     })
